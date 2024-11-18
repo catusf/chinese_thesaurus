176556dac3ae85318836039daa89cf6d35e97095
catus felis <phanmanhdan@gmail.com>
2024-11-18 13:44:15 +0000

    Fixes formatting

--- make_synonym_antonym.py
+++ make_synonym_antonym.py
@@ -5,8 +5,6 @@ from pinyin import get as pinyinget
 
 # searcher = HanziDictionary()
 # result = searcher.definition_lookup("好", script_type="simplified")
-
-
 # print(result)
 
 # Initialize the synonym dictionary with defaultdict
@@ -19,7 +17,7 @@ MAX_ITEMS = 100000 # For debug purposes
 BIG_ITEMS = 50 # For debug purposes
 
 PC_NEWLINE = chr(0xEAB1)
-PC_RIGHT_TRIANGLE = "▸"  # ▸
+PC_RIGHT_TRIANGLE = "»"  # ▸
 
 def pleco_make_bold(text):
     return f"{chr(0xEAB2)}{text}{chr(0xEAB3)}"
@@ -28,20 +26,16 @@ def pleco_make_link(text):
     return f"{chr(0xEAB8)}{text}{chr(0xEABB)}"
 
 def make_linked_items(cur_item, list_items):
-    contents = f"{PC_RIGHT_TRIANGLE} "
-
     item_set = set(list_items)
     item_set.discard(cur_item) # Dont repeat the headword
 
     items = sorted(list(item_set))
+    contents = ""
 
     for line in items:
-        words = line.split(' ')
-
-        for word in words:
-            contents += f"{pleco_make_link(word)} "
-
-        contents += '\n'
+        words = [(pleco_make_link(word) + " " + pinyinget(word)) for word in line.split(' ')]
+        contents += f"{PC_RIGHT_TRIANGLE} {" ".join(words)}。\n"
+    
     return contents
 
 # Process the file line by line
@@ -191,7 +185,7 @@ with open('data/SND-Pleco.txt', 'w', encoding='utf-8') as snd_file:
         negations = set(thesaurus_dict[item]["NegationSet"])
 
         if not(len(antonyms) + len(synonyms) + len(negations)):
-            print(f"{item} does not have any thesaurus items.")
+            # print(f"{item} does not have any thesaurus items.")
 
             continue
 
